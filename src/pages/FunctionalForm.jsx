b7ab008ddea9e26508c0070a606d09e34d53cbaf
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-21 10:00:27 -0300

    rebuild: single quote changed to double quote

--- src/pages/FunctionalForm.jsx
+++ src/pages/FunctionalForm.jsx
@@ -1,8 +1,8 @@
 // The construction of this page was based on:
 // https://react-hook-form.com/get-started/
 
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const FuncionalForm = () => {
   const { register, handleSubmit } = useForm();
@@ -17,28 +17,28 @@ const FuncionalForm = () => {
           </div>
           <div>
             <button
-              data-testId='button-save'
-              type='submit'
-              disabled='false'
+              data-testId="button-save"
+              type="submit"
+              disabled="false"
             >
               Salvar
             </button>
           </div>
         </header>
-        <input {...register('name')} placeholder='Nome *' data-testId='input-name' />
-        <input {...register('initialDate')} placeholder='Data Inicial *' data-testId='input-initial-date'/>
-        <input {...register('finalDate')} placeholder='Data Final *' data-testId='input-final-date'/>
-        <select {...register('properties')} placeholder='Propriedade *' data-testId='select-properties' >
-          <option value='1'>Propriedade 1</option>
-          <option value='2'>Propriedade 2</option>
-          <option value='3'>Propriedade 3</option>
+        <input {...register("name")} placeholder="Nome *" data-testId="input-name" />
+        <input {...register("initialDate")} placeholder="Data Inicial *" data-testId="input-initial-date"/>
+        <input {...register("finalDate")} placeholder="Data Final *" data-testId="input-final-date"/>
+        <select {...register("properties")} placeholder="Propriedade *" data-testId="select-properties" >
+          <option value="1">Propriedade 1</option>
+          <option value="2">Propriedade 2</option>
+          <option value="3">Propriedade 3</option>
         </select>
-        <select {...register('laboratory')} placeholder='Laboratório *' data-testId='select-laboratory' >
-          <option value='1'>Laboratório 1</option>
-          <option value='2'>Laboratório 2</option>
-          <option value='3'>Laboratório 3</option>
+        <select {...register("laboratory")} placeholder="Laboratório *" data-testId="select-laboratory" >
+          <option value="1">Laboratório 1</option>
+          <option value="2">Laboratório 2</option>
+          <option value="3">Laboratório 3</option>
         </select>
-        <textarea {...register('observations')} placeholder='Observações' data-testId='textarea-observations' />
+        <textarea {...register("observations")} placeholder="Observações" data-testId="textarea-observations" />
         
       </form>
     </section>
