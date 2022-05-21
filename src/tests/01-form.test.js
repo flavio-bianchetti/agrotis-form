d0fb82cac04a41a1fb931a70d26d53f87119e178
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-21 08:41:10 -0300

    tests: Form test file methods updated.

--- src/tests/01-form.test.js
+++ src/tests/01-form.test.js
@@ -2,63 +2,71 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouterProvider from '../renderWithRouterProvider';
+import {render as rtlRender, screen} from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// solução encontrada em:
+// https://testing-library.com/docs/example-codesandbox/
+const render = (component, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+  return rtlRender(component, { wrapper: Router });
+};
 
 describe('1 - Renderiza a página do formulário', () => {
   it ('o título está presente', () => {
-    const { getByText } = renderWithRouterProvider(<App />);
-    expect(getByText(/teste front-end/i)).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByText(/teste front-end/i)).toBeInTheDocument();
   });
 
   it('o campo "Nome" está presente', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('input-name')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByTestId('input-name')).toBeInTheDocument();
   });
 
   it('o campo "Data Inicial" está presente', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('input-initial-date')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByTestId('input-initial-date')).toBeInTheDocument();
   });
 
   it('o campo "Data Final" está presente', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('input-final-date')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByTestId('input-final-date')).toBeInTheDocument();
   });
 
   it('o campo "Propriedades" está presente', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('select-properties')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByTestId('select-properties')).toBeInTheDocument();
   });
 
   it('o campo "Laboratório" está presente', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('select-laboratory')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByTestId('select-laboratory')).toBeInTheDocument();
   });
 
   it('o campo "Observações" está presente', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('textarea-observations')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByTestId('textarea-observations')).toBeInTheDocument();
   });
 
   it('o botão "SALVAR" está presente', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('button-save')).toBeInTheDocument();
+    render(<App />);
+    expect(screen.getByTestId('button-save')).toBeInTheDocument();
   });
 
   it('o botão "SALVAR" está desabilitado', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    expect(getByTestId('button-save')).toBeDisabled();
+    render(<App />);
+    expect(screen.getByTestId('button-save')).toBeDisabled();
   });
 
   it('o botão "SALVAR" fica habilitado após a inserção das informações', () => {
-    const { getByTestId } = renderWithRouterProvider(<App />);
-    const name = getByTestId('input-name');
-    const initialDate = getByTestId('input-initial-date');
-    const finalDate = getByTestId('input-final-date');
-    const properties = getByTestId('select-properties');
-    const laboratory = getByTestId('select-laboratory');
-    const observations = getByTestId('textarea-observations');
-    const button = getByTestId('button-save');
+    render(<App />);
+    const name = screen.getByTestId('input-name');
+    const initialDate = screen.getByTestId('input-initial-date');
+    const finalDate = screen.getByTestId('input-final-date');
+    const properties = screen.getByTestId('select-properties');
+    const laboratory = screen.getByTestId('select-laboratory');
+    const observations = screen.getByTestId('textarea-observations');
+    const button = screen.getByTestId('button-save');
     userEvent.type(name, 'Sebastião Ferreira Maia');
     userEvent.type(initialDate, '01/01/2022');
     userEvent.type(finalDate, '01/01/2023');
