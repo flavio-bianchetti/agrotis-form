c53067f29790ca21e24ab56e0f9fddaefdc966cd
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-22 10:38:13 -0300

    feat: Select methods & attributes included

--- src/context/AgrotisProvider.jsx
+++ src/context/AgrotisProvider.jsx
@@ -2,11 +2,18 @@ import PropTypes from "prop-types";
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import AgrotisContext from "./AgrotisContext";
+import { infosPropriedades, infosLaboratorios } from "../data/AgrotisDB";
 
 const AgrotisProvider = ({ children }) => {
   const { register, handleSubmit } = useForm();
+  const [dataProperties, setDataProperties] = useState([]);
+  const [dataLaboratories, setDataLaboratories] = useState([]);
+  const [properties, setProperties] = React.useState("");
+  const [laboratory, setLaboratory] = React.useState("");
   
   useEffect(() => {
+    setDataProperties(infosPropriedades);
+    setDataLaboratories(infosLaboratorios);
   }, []);
 
   const onSubmit = (data, event) => {
@@ -21,9 +28,23 @@ const AgrotisProvider = ({ children }) => {
     console.log("event", event);
   }
 
+  const handleChangeProperties = (event) => {
+    setProperties(event.target.value);
+  };
+
+  const handleChangeLaboratory = (event) => {
+    setLaboratory(event.target.value);
+  };
+
   const agrotisValues = {
     register,
     handleSubmit,
+    dataProperties,
+    dataLaboratories,
+    properties,
+    laboratory,
+    handleChangeProperties,
+    handleChangeLaboratory,
 };
 
   return (
