7d3d9fefce4b051100c59bbaea146bc49cf2b71d
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-22 10:32:23 -0300

    feat: react-hook-form included in Provider

--- src/context/AgrotisProvider.jsx
+++ src/context/AgrotisProvider.jsx
@@ -1,12 +1,29 @@
 import PropTypes from "prop-types";
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import AgrotisContext from "./AgrotisContext";
 
 const AgrotisProvider = ({ children }) => {
+  const { register, handleSubmit } = useForm();
+  
   useEffect(() => {
   }, []);
 
+  const onSubmit = (data, event) => {
+    console.log("data", data);
+    console.log("event", event);
+    console.log("register", register);
+  };
+  
+  const onError = (event) => {
+    console.log("wrong");
+    setIsNameError(true);
+    console.log("event", event);
+  }
+
   const agrotisValues = {
+    register,
+    handleSubmit,
 };
 
   return (
