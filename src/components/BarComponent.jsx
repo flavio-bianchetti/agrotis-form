fcaec64c758d43786700470687e0732a2cba2289
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-22 10:23:24 -0300

    fix: Singles quote changed to doubles quote

--- src/components/BarComponent.jsx
+++ src/components/BarComponent.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { AppBar, Stack, Typography, Button  } from "@mui/material";
 import { Container } from "@mui/system";
 
