33c50b7830dd8d5cea745b4053c79086d3bf169e
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-23 00:56:20 -0300

    tests: refactored tests

--- src/tests/01-form.test.js
+++ src/tests/01-form.test.js
@@ -1,78 +1,77 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
-import {render as rtlRender, screen} from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import { screen } from "@testing-library/react";
+import renderWithRouterAndProvider from "../renderWithRouterAndProvider";
 
-// solução encontrada em:
-// https://testing-library.com/docs/example-codesandbox/
-const render = (component, { route = '/' } = {}) => {
-  window.history.pushState({}, 'Test page', route);
-  return rtlRender(component, { wrapper: Router });
-};
+describe("1 - Renderiza a página do formulário", () => {
 
-describe('1 - Renderiza a página do formulário', () => {
-  it ('o título está presente', () => {
-    render(<App />);
-    expect(screen.getByText(/teste front-end/i)).toBeInTheDocument();
+  it ("o título está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByText("Teste front-end")).toBeInTheDocument();
   });
 
-  it('o campo "Nome" está presente', () => {
-    render(<App />);
-    expect(screen.getByTestId('input-name')).toBeInTheDocument();
+  it("o campo 'Nome' está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByTestId("input-name")).toBeInTheDocument();
   });
 
-  it('o campo "Data Inicial" está presente', () => {
-    render(<App />);
-    expect(screen.getByTestId('input-initial-date')).toBeInTheDocument();
+  it("o campo 'Data Inicial' está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByTestId("input-initial-date")).toBeInTheDocument();
   });
 
-  it('o campo "Data Final" está presente', () => {
-    render(<App />);
-    expect(screen.getByTestId('input-final-date')).toBeInTheDocument();
+  it("o campo 'Data Final' está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByTestId("input-final-date")).toBeInTheDocument();
   });
 
-  it('o campo "Propriedades" está presente', () => {
-    render(<App />);
-    expect(screen.getByTestId('select-properties')).toBeInTheDocument();
+  it("o campo 'Propriedades' está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByTestId("select-properties")).toBeInTheDocument();
   });
 
-  it('o campo "Laboratório" está presente', () => {
-    render(<App />);
-    expect(screen.getByTestId('select-laboratory')).toBeInTheDocument();
+  it("o campo 'Laboratório' está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByTestId("select-laboratory")).toBeInTheDocument();
   });
 
-  it('o campo "Observações" está presente', () => {
-    render(<App />);
-    expect(screen.getByTestId('textarea-observations')).toBeInTheDocument();
+  it("o campo 'Observações' está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByTestId("textarea-observations")).toBeInTheDocument();
   });
 
-  it('o botão "SALVAR" está presente', () => {
-    render(<App />);
-    expect(screen.getByTestId('button-save')).toBeInTheDocument();
+  it("o botão 'SALVAR' está presente", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByText(/salvar/i)).toBeInTheDocument();
+  });
+});
+
+describe("2 - Testa o formulário", () => {
+  it("o formulário está vazio", () => {
+    renderWithRouterAndProvider(<App />);
+    expect(screen.getByTestId("input-name").value).toBe("");
+    expect(screen.getByTestId("input-initial-date").value).toBe("");
+    expect(screen.getByTestId("input-final-date").value).toBe("");
+    expect(screen.getByTestId("select-properties").value).toBe("");
+    expect(screen.getByTestId("select-laboratory").value).toBe("");
+    expect(screen.getByTestId("textarea-observations").value).toBe("");
   });
 
-  it('o botão "SALVAR" está desabilitado', () => {
-    render(<App />);
-    expect(screen.getByTestId('button-save')).toBeDisabled();
-  });
-
-  it('o botão "SALVAR" fica habilitado após a inserção das informações', () => {
-    render(<App />);
-    const name = screen.getByTestId('input-name');
-    const initialDate = screen.getByTestId('input-initial-date');
-    const finalDate = screen.getByTestId('input-final-date');
-    const properties = screen.getByTestId('select-properties');
-    const laboratory = screen.getByTestId('select-laboratory');
-    const observations = screen.getByTestId('textarea-observations');
-    const button = screen.getByTestId('button-save');
-    userEvent.type(name, 'Sebastião Ferreira Maia');
-    userEvent.type(initialDate, '01/01/2022');
-    userEvent.type(finalDate, '01/01/2023');
-    userEvent.type(properties, 'Agrotis 1');
-    userEvent.type(laboratory, 'Agro Skynet');
-    userEvent.type(observations, 'Não há observações');
-    expect(button).not.toBeDisabled();
+  it("o formulário está preenchido", () => {
+    renderWithRouterAndProvider(<App />);
+    userEvent.type(screen.getByTestId("input-name"), "Teste");
+    userEvent.type(screen.getByTestId("input-initial-date"), "01/01/2020");
+    userEvent.type(screen.getByTestId("input-final-date"), "01/01/2020");
+    userEvent.selectOptions(screen.getByTestId("select-properties"), "1");
+    userEvent.selectOptions(screen.getByTestId("select-laboratory"), "1");
+    userEvent.type(screen.getByTestId("textarea-observations"), "Teste");
+    expect(screen.getByTestId("input-name").value).toBe("Teste");
+    expect(screen.getByTestId("input-initial-date").value).toBe("01/01/2020");
+    expect(screen.getByTestId("input-final-date").value).toBe("01/01/2020");
+    expect(screen.getByTestId("select-properties").value).toBe("1");
+    expect(screen.getByTestId("select-laboratory").value).toBe("1");
+    expect(screen.getByTestId("textarea-observations").value).toBe("Teste");
   });
 });
